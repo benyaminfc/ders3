0265e7a6589261c624d8697b6e5eee86119cdd66
mehmet <mehmet.imdat0@gmail.com>
2022-06-06 19:09:28 +0300

    firsttwo

--- src/main/kotlin/packed/veriables.kt
+++ src/main/kotlin/packed/veriables.kt
@@ -1,2 +1,27 @@
 package packed
 
+fun main() {
+    val name : String = "mehmet"
+    var surName : String = "imdat"
+
+
+    surName="iliksiz"
+   //    name="ali"
+
+    println(name)
+    print(surName)
+
+    val age = 20
+
+    println(age)
+
+    val eyeColor : String
+    eyeColor = "blue"
+    println(eyeColor)
+
+
+  //  var skinTane              ÇALIŞMAZ
+  //  var skinTane="white"
+
+
+}
